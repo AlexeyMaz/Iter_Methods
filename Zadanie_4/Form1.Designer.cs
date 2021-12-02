9b86d479a0cd0a8768455b5e3d11fea04f0e7c10
Алексей Андреевич Мазницкий <alexmaznitskiy@gmail.com>
2021-12-02 08:53:26 +0300

    Добавьте файлы проекта.

---
+++ Zadanie_4/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1217, 25);
+            this.label1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Матрица А";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1226, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 160);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1468, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(415, 43);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Вектор свободных членов";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1477, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 160);
+            this.label4.TabIndex = 4;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(25, 34);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1165, 777);
+            this.zedGraphControl1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1217, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(280, 43);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Введите точность";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(1223, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 39);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(1217, 433);
+            this.label7.MaximumSize = new System.Drawing.Size(200, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 43);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Решение:";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(1368, 433);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(528, 42);
+            this.label8.TabIndex = 10;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(1545, 486);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(202, 42);
+            this.label12.TabIndex = 15;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(1217, 486);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(322, 43);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Количество итераций: ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1217, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 54);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Решить методом Якоби";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1217, 547);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(594, 54);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Решить методом минимальных невязок";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1545, 719);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 42);
+            this.label6.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(1217, 719);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(322, 43);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Количество итераций: ";
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(1368, 622);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(528, 85);
+            this.label10.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(1217, 622);
+            this.label11.MaximumSize = new System.Drawing.Size(200, 200);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(173, 43);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Решение:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1861, 869);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private Label label5;
+        private TextBox textBox1;
+        private Label label7;
+        private Label label8;
+        private Label label12;
+        private Label label13;
+        private Button button1;
+        private Button button2;
+        private Label label6;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
+
---
+++ Zadanie_4/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using ZedGraph;
+
+namespace Zadanie_4
+{
+    public partial class Form1 : Form
+    {
+        double eps;
+        int n;
+        double[] b;
+        double[,] A;
+        public Form1()
+        {
+            InitializeComponent();
+            OnLoad();
+        }
+        static double[,] Input(out int n, out double[] b)
+        {
+            string s;
+            n = -1;
+            using (StreamReader file = new StreamReader("f.txt"))
+            {
+                s = file.ReadToEnd();
+            }
+            using (StreamReader file = new StreamReader("f.txt"))
+            {
+                string tmp;
+                do
+                {
+                    tmp = file.ReadLine();
+                    n++;
+                } while (tmp != null);
+            }
+            string[] stroka = s.Split('\n');
+            string[] stolbec = stroka[0].Split(' ');
+            double[,] a = new double[n, n];
+            b = new double[n];
+            int t;
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                stolbec = stroka[i].Split(' ');
+                for (int j = 0; j < stolbec.Length - 1; j++)
+                {
+                    t = Convert.ToInt32(stolbec[j]);
+                    a[i, j] = t;
+                }
+                t = Convert.ToInt32(stolbec[n]);
+                b[i] = t;
+            }
+            return a;
+        }
+
+        void OnLoad()
+        {
+            n = 0;
+            b = new double[n];
+            A = Input(out n, out b);
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                    label2.Text += Convert.ToString(A[i, j]) + "  ";
+                label2.Text += "\n";
+                label4.Text += Convert.ToString(b[i]) + "\n";
+            }
+
+
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "График зависимости нормы невязки от номера итерации";
+            myPane.XAxis.Title.Text = "Номер итерации";
+            myPane.YAxis.Title.Text = "Значение нормы невязки";
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            //myPane.YAxis.MinorGrid.IsVisible = true;
+            myPane.XAxis.MinorGrid.IsVisible = true;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 1;
+        }
+
+        double residual_norm(double[] x)
+        {
+            double[] r = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                double s = 0;
+                for (int j = 0; j < n; j++)
+                    s += A[i, j] * x[j];
+                r[i] = s - b[i];
+            }
+
+            double norm = 0;
+            for (int i = 0; i < n; i++)
+                norm += r[i] * r[i];
+            norm = Math.Sqrt(norm);
+            return norm;
+        }
+
+        double[] Jacobi(out int numberOfIter)
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            PointPairList list = new PointPairList();
+            numberOfIter = 0;
+            double[] TempX = new double[n];
+            double norm;
+            double[] x = new double[n];
+            for (int i = 0; i < n; i++)
+                x[i] = 1;
+
+            do
+            {
+                list.Add(numberOfIter, residual_norm(x));
+                for (int i = 0; i < n; i++)
+                {
+                    TempX[i] = b[i];
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (i != k)
+                            TempX[i] -= A[i, k] * x[k];
+                    }
+                    TempX[i] /= A[i, i];
+                }
+                norm = Math.Abs(x[0] - TempX[0]);
+                for (int i = 0; i < n; i++)
+                {
+                    if (Math.Abs(x[i] - TempX[i]) > norm)
+                        norm = Math.Abs(x[i] - TempX[i]);
+                    x[i] = TempX[i];
+                }
+                numberOfIter++;
+            } while (norm > eps);
+
+            list.Add(numberOfIter, residual_norm(x));
+            LineItem myCurve = myPane.AddCurve("Jacobi", list, Color.Blue, SymbolType.Circle);
+            return x;
+        }
+
+        double[] Matr_prod_Vector(double[] vector)
+        {
+            double[] R = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                R[i] = 0;
+                for (int j = 0; j < n; j++)
+                    R[i] += A[i,j] * vector[j];
+            }
+            return R;
+        }
+        double[] MinResid(out int numberOfIter)
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            PointPairList list = new PointPairList();
+            double[] x = new double[n];
+            double[] TempX = new double[n];
+            double[] Delta = new double[n];
+            double[] R;
+            double norm, Tau = 0, TempTau = 0;
+            numberOfIter = 0;
+            for (int i = 0; i < n; i++)
+                TempX[i] = 1;
+
+            do
+            {
+                list.Add(numberOfIter, residual_norm(TempX));
+                R = Matr_prod_Vector(TempX);
+                for (int i = 0; i < n; i++)
+                {
+                    Delta[i] = R[i] - b[i];
+                }
+                R = Matr_prod_Vector(Delta);
+                Tau = 0;
+                TempTau = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Tau += R[i] * Delta[i];
+                    TempTau += R[i] * R[i];
+                }
+                Tau /= TempTau;
+                for (int i = 0; i < n; i++)
+                    x[i] = TempX[i] - Tau * Delta[i];
+                norm = Math.Abs(x[0] - TempX[0]);
+                for (int i = 0; i < n; i++)
+                {
+                    if (Math.Abs(x[i] - TempX[i]) > norm)
+                        norm = Math.Abs(x[i] - TempX[i]);
+                    TempX[i] = x[i];
+                }
+                numberOfIter++;
+            } while (norm >= eps);
+
+            list.Add(numberOfIter, residual_norm(TempX));
+            LineItem myCurve = myPane.AddCurve("MinResid", list, Color.Red, SymbolType.Circle);
+            return x;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            eps = Convert.ToDouble(textBox1.Text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iter_count;
+            double[] x = Jacobi(out iter_count);
+            label8.Text = "";
+            for (int i = 0; i < n; i++)
+                label8.Text += x[i] + "  ";
+            label12.Text = Convert.ToString(iter_count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int iter_count;
+            double[] x = MinResid(out iter_count);
+            label10.Text = "";
+            for (int i = 0; i < n; i++)
+                label10.Text += x[i] + "  ";
+            label6.Text = Convert.ToString(iter_count);
+
+        }
+    }
+}
